5a82edb3c800ca506e368f0f63bc2addc448a0da
musabrt <musafg@outlook.com>
2021-11-10 22:15:32 +0300

    Implements Iterable the FixedStack

--- src/me/koply/nplayer/util/FixedStack.java
+++ src/me/koply/nplayer/util/FixedStack.java
@@ -1,8 +1,12 @@
 package me.koply.nplayer.util;
 
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
 // if stack is full
 // overwrites last index
-public class FixedStack<T> {
+public class FixedStack<T> implements Iterable<T> {
 
     private final Object[] array;
     public final int size;
@@ -21,6 +25,12 @@ public class FixedStack<T> {
         else cursor++;
     }
 
+    public void addAll(T...arr) {
+        for (T obj : arr) {
+            push(obj);
+        }
+    }
+
     @SuppressWarnings("unchecked")
     public T pop() {
         if (cursor == 0) cursor = size-1;
@@ -28,5 +38,39 @@ public class FixedStack<T> {
         Object toreturn = array[cursor];
         array[cursor] = null;
         return (T) toreturn;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        // final wrapper for anonymous class
+        final int lastCursor = cursor;
+        return new Iterator<>() {
+            int _cursor = lastCursor;
+            int i = 0;
+            @Override
+            public boolean hasNext() {
+                if (i==size) return false;
+                i++;
+                if (_cursor==0) _cursor = size-1;
+                else _cursor--;
+                return array[_cursor] != null;
+            }
+
+            @SuppressWarnings("unchecked")
+            @Override
+            public T next() {
+                return (T) array[_cursor];
+            }
+        };
+    }
+
+    @Override
+    public void forEach(Consumer<? super T> action) {
+        Iterable.super.forEach(action);
+    }
+
+    @Override
+    public Spliterator<T> spliterator() {
+        return Iterable.super.spliterator();
     }
 }
